79b8bb44b8eef1b1ee21e072fd29e7144efb25b5
chen <chen@10.2.5.173>
2019-09-05 08:44:48 -0700

    Turning it in!

--- Squirtle/src/SquirtleBoard.java
+++ Squirtle/src/SquirtleBoard.java
@@ -22,10 +22,17 @@ public class SquirtleBoard extends Board {
 	 	squirtle.setLocation(0, 0);
 	 	add(squirtle);
 	 	
+	 	SquirtleThing squirtle2 = new SquirtleThing();
+	  	squirtle2.setLocation(3, 1);
+	  	add(squirtle2);
+	 	
 	 	// Create a BulbasaurThing at 0, 4
 	 	BulbasaurThing bulbasaur = new BulbasaurThing();
 	 	bulbasaur.setLocation(4, 0);
 	 	add(bulbasaur);
 	 	
+	 	BulbasaurThing bulbasaur2 = new BulbasaurThing();
+	 	bulbasaur2.setLocation(5, 2);
+	 	add(bulbasaur2);
 	}
 } 
